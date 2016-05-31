f47fd5b221d89ad4d1530872bfc64add4077ef1f
Nikhil Kathuria <nikhil.kathuria@gmail.com>
2016-05-31 10:26:55 -0400

    Added the extract data and stats functionality

---
+++ trec/ExtractData.py
@@ -0,0 +1,95 @@
+from glob import glob
+import json
+import os
+
+
+import streamcorpus
+from Cleanse import *
+
+inpath="/Users/nikhilk/Documents/NEU_MSCS/MLLAB/Text_Vectors/trec"
+outpath="/Users/nikhilk/Documents/NEU_MSCS/MLLAB/Text_Vectors/trec/data"
+
+
+
+def dump_tfmap(qmap, fname, idt):
+    with open(fname, 'w') as outfile:
+        json.dump(qmap, indent=idt, separators=(',', ': '), fp=outfile)
+
+
+def dump_dfmap(qmap, fname):
+    dfmap = {term: len(qmap[term]) for term in qmap}
+    dump_tfmap(dfmap, fname, 1)
+
+
+def updatemap(qmap, word, docno):
+    if word in qmap:
+        dmap = qmap[word]
+        if docno in dmap:
+            dmap[docno] += 1
+        else:
+            dmap[docno] = 0
+    else:
+        mydict = {docno : 1}
+        qmap[word] = mydict
+
+
+def extract(fname, corpus, out):
+    qmap = dict()
+
+    fobj = open(fname, 'w')
+
+    for si in streamcorpus.Chunk(path=corpus):
+        docno = si.doc_id
+
+        ## get the tag-stripped text from 'clean_visible'
+        if si.body.clean_visible is None:
+            continue
+
+        # Here we concatenate the words into a space-separated string. Saves us the
+        # hassle of cleaning and tokenizing the clean_visible text.
+        try:
+            sentences = si.body.sentences["serif"]
+        except KeyError:
+            sentences = si.body.sentences["lingpipe"]
+
+        # One entry per sentence. Could concatenate further if desired.
+        # sentencesa = [" ".join([x.token for x in s.tokens]) for s in sentences]
+        for s in sentences:
+            sent = ""
+            for x in s.tokens:
+                # word = replaceurl(onlyenglish(x.token)).lower()
+                word = replaceurl(replacemail(x.token))
+                word = remove_(exceptwords(word)).lower()
+
+                # Update the IR map
+                updatemap(qmap, word, docno)
+
+                sent = sent + " " + word
+            fobj.write(shrinkspace(sent).strip() + " ")
+
+    # Post processing
+    fobj.close()
+
+    tf_file = out + "/" + "TFmap.json"
+    df_file = out + "/" + "DFmap.sjon"
+    print tf_file
+    dump_tfmap(qmap, tf_file, 2)
+    dump_dfmap(qmap, df_file)
+
+
+def read():
+    for filename in glob(inpath + '/*'):
+        spa = filename.split('/')
+        dirname = spa[len(spa) - 1].split('.')[0]
+
+        out = outpath + "/" + dirname
+        os.makedirs(out)
+        fileout = 'data.txt'
+
+
+        extract(fileout, filename, out)
+
+
+
+if __name__ == '__main__':
+    read()
